d171bfd677bb9540c5e6090689cae38e7398ac57
Irfandi Riawan <irfandi.riawan@yahoo.com>
2025-01-13 16:38:12 +0700

    Add header and footer files

---
+++ headfoot.js
@@ -0,0 +1,12 @@
+function loadHTML(file, elementId) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Load the header and footer
+loadHTML('header.html', 'header');
+loadHTML('footer.html', 'footer');
